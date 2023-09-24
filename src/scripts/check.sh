9f9df6dd69564d746723866a9d10fd74428cd59a
Andrea Reverberi <andrea.reverberi@gatech.edu>
2023-09-24 20:33:06 +0200

    fix: estratti comandi in script

---
+++ src/scripts/check.sh
@@ -0,0 +1,9 @@
+if (npm run -s check-versions | grep -qF "$PACKAGE_NAME") ; then
+    echo "package $PACKAGE_NAME found, executing the rest of the steps"
+    npm run app:version
+    echo "export $TAG_VARIABLE=$(npm run current-version -w=$FOLDER --silent)" >> "$BASH_ENV"
+    source "$BASH_ENV"
+else
+    echo "package $PACKAGE_NAME not tagged. Stopping the job"
+    circleci-agent step halt
+fi
